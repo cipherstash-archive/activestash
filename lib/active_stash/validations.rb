ec3758650c3fbcd153b669651bcd5088ddf1fe48
Dan Draper <dan@coderdan.co>
2022-07-26 20:56:59 +1000

    Handle additional options from the standard validator

--- lib/active_stash/validations.rb
+++ lib/active_stash/validations.rb
@@ -1,11 +1,19 @@
 module ActiveStash
   module Validations
-     class UniquenessValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      if record.class.query(attribute => value).exists?
-        record.errors.add(attribute, options[:message] || "already exists")
+    class UniquenessValidator < ActiveModel::EachValidator
+
+      # Uniqueness validator for encryped fields.
+      #
+      # It relies on an exact index being present for the
+      # attribute (created by default with `stash_index`)
+      #
+      def validate_each(record, attribute, value)
+        result = record.class.query(attribute => value).first
+
+        if (options[:case_sensitive] && result[attribute] == attribute) || result
+          record.errors.add(attribute, options[:message] || "already exists")
+        end
       end
     end
-  end
   end
 end
--- spec/support/user2.rb
+++ spec/support/user2.rb
@@ -1,10 +1,11 @@
 class User2 < ActiveRecord::Base
   include ActiveStash::Search
+  include ActiveStash::Validations
   self.table_name = "users2"
   self.collection_name = "activestash_test_#{ENV["ACTIVE_STASH_TEST_COLLECTION_PREFIX"] || ""}_users2"
 
-  stash_index :first_name
-  stash_index :dob, :last_name
+  validates_uniqueness_of :email, case_sensitive: true
+  stash_index :first_name, :email, :dob, :last_name
   stash_index :gender, only: :exact
   stash_index :title, except: [:match, :range]
   stash_index :created_at, :updated_at, except: :range

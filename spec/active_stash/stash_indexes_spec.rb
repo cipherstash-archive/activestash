865b3134eb0a4852f2710219f3549bc74757358d
Matt Palmer <matt@cipherstash.com>
2022-08-03 11:41:24 +1000

    Extract out custom matchers into their own file

--- spec/active_stash/stash_indexes_spec.rb
+++ spec/active_stash/stash_indexes_spec.rb
@@ -1,28 +1,7 @@
 require_relative "../support/user2"
 require_relative "../support/migrations/create_users2"
 
-require 'rspec/expectations'
-
-RSpec::Matchers.define :have_an_exact_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :exact }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec::Matchers.define :have_a_range_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :range }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec::Matchers.define :have_a_match_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :match }
-    !target.nil? && target.name == name
-  end
-end
+require_relative "../support/matchers"
 
 RSpec.describe ActiveStash::StashIndexes do
   before(:all) do

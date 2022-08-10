c9c7ae536461af1c6cd4925d43845c0d65e6a9df
fimac <fiona@cipherstash.com>
2022-08-11 08:29:16 +1000

    add test to cover error raised on duplicates and case sensitivity

--- spec/put_spec.rb
+++ spec/put_spec.rb
@@ -1,5 +1,7 @@
 require_relative "support/user"
+require_relative "support/user_unique_indexes"
 require_relative "support/migrations/create_users"
+require_relative "support/migrations/create_users2"
 
 RSpec.describe "ActiveStash::Search.cs_put" do
   describe "#cs_put" do
@@ -21,7 +23,7 @@ RSpec.describe "ActiveStash::Search.cs_put" do
         dob: "Aug 3, 1963",
         created_at: 10.days.ago,
         title: "Mr",
-        email: "james@metalica.net"
+        email: "james@metallica.net"
       )
     end
 
@@ -36,5 +38,95 @@ RSpec.describe "ActiveStash::Search.cs_put" do
       expect(user.stash_id).not_to be_nil
     end
   end
-end
 
+  describe "#cs_put with unique cs indexes with a unique constraint on email" do
+    before(:all) do
+      CreateUsers2.migrate(:up)
+    end
+
+    after(:all) do
+      CreateUsers2.migrate(:down)
+    end
+
+    before(:each) do
+      UserUniqueIndexes.collection.create!
+    end
+
+    after(:each) do
+      UserUniqueIndexes.delete_all
+      UserUniqueIndexes.collection.drop!
+    end
+
+    it "should raise an error on insert of duplicate" do
+      UserUniqueIndexes.create!(
+        first_name: "Selina",
+        last_name: "Meyer",
+        gender: "F",
+        dob: "Jan 1, 1961",
+        created_at: 10.days.ago,
+        title: "Ms",
+        email: "smeyer@veep.net"
+      )
+
+      expect do
+        UserUniqueIndexes.create!(
+          first_name: "Serena",
+          last_name: "Meyer",
+          gender: "F",
+          dob: "Jan 1, 1962",
+          created_at: 10.days.ago,
+          title: "Ms",
+          email: "smeyer@veep.net"
+        )
+      end.to raise_error(CipherStash::Client::Error::RecordPutFailure)
+    end
+
+    it "an error will be raised if the email is in a different case" do
+      UserUniqueIndexes.create!(
+        first_name: "Selina",
+        last_name: "Meyer",
+        gender: "F",
+        dob: "Jan 1, 1961",
+        created_at: 10.days.ago,
+        title: "Ms",
+        email: "smeyer@veep.net"
+      )
+
+      expect do
+        UserUniqueIndexes.create!(
+          first_name: "Serena",
+          last_name: "Meyer",
+          gender: "F",
+          dob: "Jan 1, 1962",
+          created_at: 10.days.ago,
+          title: "Ms",
+          email: "SMEyer@Veep.net"
+        )
+      end.to raise_error(CipherStash::Client::Error::RecordPutFailure)
+    end
+
+    it "does not raise an error if the email is not a duplicate" do
+       UserUniqueIndexes.create!(
+        first_name: "Selina",
+        last_name: "Meyer",
+        gender: "F",
+        dob: "Jan 1, 1961",
+        created_at: 10.days.ago,
+        title: "Ms",
+        email: "smeyer@veep.net"
+      )
+
+      expect do
+        UserUniqueIndexes.create!(
+          first_name: "Serena",
+          last_name: "Meyer",
+          gender: "F",
+          dob: "Jan 1, 1962",
+          created_at: 10.days.ago,
+          title: "Ms",
+          email: "smeyers@veep.net"
+        )
+      end.not_to raise_error()
+    end
+  end
+end

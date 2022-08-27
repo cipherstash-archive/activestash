edf4025bdef74d5b98210c1acc07ef187bf90ec9
Drew Thomas <drew@cipherstash.com>
2022-08-27 12:35:34 +1000

    Add more column name rules and refactor specs

--- spec/active_stash/assess/column_name_rules_spec.rb
+++ spec/active_stash/assess/column_name_rules_spec.rb
@@ -1,23 +1,56 @@
 RSpec.describe ActiveStash::Assess::ColumnNameRules do
   describe ".check" do
     [
-      [%w(name), "names"],
-      [%w(lastname lname surname), "last names"],
-      [%w(phone phonenumber), "phone numbers"],
-      [%w(dateofbirth birthday dob), "dates of birth"],
-      [%w(zip zipcode postalcode), "postal codes"],
-      [%w(accesstoken refreshtoken), "OAuth tokens"],
-    ].each do |field_names, expected_display_name|
-      it "finds a match for all fields given #{field_names.inspect}" do
-        matches = described_class.check(field_names)
-        expect(matches.keys.sort).to eq(field_names.sort)
+      ["name", "names", "AS0001"],
+      ["lastname", "names", "AS0001"],
+      ["last_name", "names", "AS0001"],
+      ["lname", "names", "AS0001"],
+      ["surname", "names", "AS0001"],
+      ["firstname", "names", "AS0001"],
+      ["first_name", "names", "AS0001"],
+      ["fname", "names", "AS0001"],
+      ["phone", "phone numbers", "AS0001"],
+      ["phone_number", "phone numbers", "AS0001"],
+      ["date_of_birth", "dates of birth", "AS0001"],
+      ["birthday", "dates of birth", "AS0001"],
+      ["dob", "dates of birth", "AS0001"],
+      ["address", "addresses", "AS0001"],
+      ["city", "addresses", "AS0001"],
+      ["state", "addresses", "AS0001"],
+      ["county", "addresses", "AS0001"],
+      ["country", "addresses", "AS0001"],
+      ["zip", "addresses", "AS0001"],
+      ["zip_code", "addresses", "AS0001"],
+      ["postal_code", "addresses", "AS0001"],
+      ["postal", "addresses", "AS0001"],
+      ["post_code", "addresses", "AS0001"],
+      ["access_token", "OAuth tokens", "AS0001"],
+      ["refresh_token", "OAuth tokens", "AS0001"],
+      ["ip", "IP addresses", "AS0001"],
+      ["ip_address", "IP addresses", "AS0001"],
+      ["ccn", "credit card numbers", "AS0001"],
+      ["credit_card_number", "credit card numbers", "AS0001"],
+      ["ssn", "social security numbers", "AS0001"],
+      ["social_security_number", "social security numbers", "AS0001"],
+      ["gender", "genders", "AS0001"],
+      ["nationality", "nationalities", "AS0001"],
+      ["tfn", "tax file numbers", "AS0001"],
+      ["tax_file_number", "tax file numbers", "AS0001"],
+      ["medicare_number", "medicare numbers", "AS0001"],
+    ].each do |field_name, expected_display_name, expected_error_code|
+      it "finds a match given field name: #{field_name.inspect}" do
+        matches = described_class.check([field_name])
+        expect(matches).to have_key(field_name)
       end
 
-      field_names.each do |field_name|
-        it "returns display name #{expected_display_name.inspect} for field name #{field_name.inspect}" do
-          matches = described_class.check(field_names)
-          expect(matches[field_name].first[:display_name]).to eq(expected_display_name)
-        end
+      it "matches for #{field_name.inspect} have the display name #{expected_display_name.inspect}" do
+        matches = described_class.check([field_name])
+        expect(matches[field_name].first[:display_name]).to eq(expected_display_name)
+      end
+
+      it "matches for #{field_name.inspect} have the error code #{expected_error_code.inspect}" do
+        matches = described_class.check([field_name])
+        expect(matches[field_name].first[:error_code]).to eq(expected_error_code)
       end
     end
   end

03c3de2e5b3d4edfb2e580cc7baac8f4bca25f9f
fimac <fiona@cipherstash.com>
2022-08-29 08:09:52 +1000

    move exact range match index test helper to spec helper file

--- spec/active_stash/unique_indexes_spec.rb
+++ spec/active_stash/unique_indexes_spec.rb
@@ -1,43 +1,9 @@
 require_relative "../support/user_unique_indexes"
 require_relative "../support/user_invalid_unique_indexes"
 require_relative "../support/migrations/create_users2"
+require_relative "../spec_helper.rb"
 
 require 'rspec/expectations'
-
-RSpec::Matchers.define :have_an_exact_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :exact }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec::Matchers.define :have_an_exact_unique_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :exact}
-    !target.nil? && target.name == name && target.unique == true
-  end
-end
-
-RSpec::Matchers.define :have_a_range_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :range }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec::Matchers.define :have_a_range_unique_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :range }
-    !target.nil? && target.name == name  && target.unique == true
-  end
-end
-
-RSpec::Matchers.define :have_a_match_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :match }
-    !target.nil? && target.name == name
-  end
-end
 
 RSpec.describe ActiveStash::StashIndexes do
   before(:all) do

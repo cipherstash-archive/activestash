92cf9db4a1a34ac42986dc61636ceb7afacb46ab
fimac <fiona@cipherstash.com>
2022-08-29 08:10:28 +1000

    run active record encryption test on rails 7 onwards only

--- spec/active_stash/active_record_encryption_spec.rb
+++ spec/active_stash/active_record_encryption_spec.rb
@@ -1,79 +1,62 @@
 require_relative "../support/user4"
 require_relative "../support/migrations/create_users4"
-
+require_relative "../spec_helper.rb"
 require 'rspec/expectations'
 
-RSpec::Matchers.define :have_an_exact_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :exact }
-    !target.nil? && target.name == name
-  end
-end
+# ActiveRecord Encryption is only available from Rails 7.
 
-RSpec::Matchers.define :have_a_range_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :range }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec::Matchers.define :have_a_match_index do |name|
-  match do |actual|
-    target = actual.find { |i| i.type == :match }
-    !target.nil? && target.name == name
-  end
-end
-
-RSpec.describe ActiveStash::StashIndexes do
-  before(:all) do
-    CreateUsers4.migrate(:up)
-  end
-
-  after(:all) do
-    CreateUsers4.migrate(:down)
-  end
-
-  let(:indexes) { User4.stash_indexes }
-
-  describe "first_name" do
-    subject { indexes.on(:first_name) }
-
-    it "has 3 indexes defined" do
-      expect(subject.length).to eq(3)
+if Rails::VERSION::MAJOR >= 7
+  RSpec.describe ActiveStash::StashIndexes do
+    before(:all) do
+      CreateUsers4.migrate(:up)
     end
 
-    it { should have_an_exact_index("first_name") }
-    it { should have_a_range_index("first_name_range") }
-    it { should have_a_match_index("first_name_match") }
-  end
-
-  describe "boolean types" do
-    subject { indexes.on(:verified) }
-
-    it "has 1 index defined" do
-      expect(subject.length).to eq(1)
+    after(:all) do
+      CreateUsers4.migrate(:down)
     end
 
-    it { should have_an_exact_index("verified") }
-  end
+    let(:indexes) { User4.stash_indexes }
 
-  describe "date types" do
-    subject { indexes.on(:dob) }
+    describe "first_name" do
+      subject { indexes.on(:first_name) }
 
-    it "has 1 index defined" do
-      expect(subject.length).to eq(1)
+      it "has 3 indexes defined" do
+        expect(subject.length).to eq(3)
+      end
+
+      it { should have_an_exact_index("first_name") }
+      it { should have_a_range_index("first_name_range") }
+      it { should have_a_match_index("first_name_match") }
     end
 
-    it { should have_a_range_index("dob_range") }
-  end
+    describe "boolean types" do
+      subject { indexes.on(:verified) }
 
-  describe "float types" do
-    subject { indexes.on(:latitude) }
+      it "has 1 index defined" do
+        expect(subject.length).to eq(1)
+      end
 
-    it "has 1 index defined" do
-      expect(subject.length).to eq(1)
+      it { should have_an_exact_index("verified") }
     end
 
-    it { should have_a_range_index("latitude_range") }
+    describe "date types" do
+      subject { indexes.on(:dob) }
+
+      it "has 1 index defined" do
+        expect(subject.length).to eq(1)
+      end
+
+      it { should have_a_range_index("dob_range") }
+    end
+
+    describe "float types" do
+      subject { indexes.on(:latitude) }
+
+      it "has 1 index defined" do
+        expect(subject.length).to eq(1)
+      end
+
+      it { should have_a_range_index("latitude_range") }
+    end
   end
 end
--- spec/support/user4.rb
+++ spec/support/user4.rb
@@ -1,8 +1,11 @@
 class User4 < ActiveRecord::Base
+  # Used to test active record encryption in on different data types.
   include ActiveStash::Search
   include ActiveStash::Validations
+
   self.table_name = "users4"
   self.collection_name = "activestash_test_#{ENV["ACTIVE_STASH_TEST_COLLECTION_PREFIX"] || ""}_users4"
+  
   encrypts :first_name, :verified, :dob, :latitude
   stash_index :first_name, :verified, :dob, :latitude
 end

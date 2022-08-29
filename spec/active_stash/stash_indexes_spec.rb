dcc9a0b0a67c7b81f3b4f96bd30941b17a1c0fbc
Matt Palmer <matt@cipherstash.com>
2022-08-29 16:11:18 +1000

    Add support for filter options in indexed fields

--- spec/active_stash/stash_indexes_spec.rb
+++ spec/active_stash/stash_indexes_spec.rb
@@ -1,7 +1,9 @@
 require_relative "../support/user2"
 require_relative "../support/user5"
+require_relative "../support/user6"
 require_relative "../support/migrations/create_users2"
 require_relative "../support/migrations/create_users5"
+require_relative "../support/migrations/create_users6"
 require_relative "../spec_helper.rb"
 
 require 'rspec/expectations'
@@ -97,5 +99,41 @@ RSpec.describe ActiveStash::StashIndexes do
         expect(subject["indexes"]["__match_multi"]["filterTermBits"]).to eq(6)
       end
     end
+  end
+
+  context "field-specific filter options" do
+    before(:all) do
+      CreateUsers6.migrate(:up)
+    end
+
+    after(:all) do
+      CreateUsers6.migrate(:down)
+    end
+
+    subject { ActiveStash::SchemaBuilder.new(User6).build }
+
+    it "passes the filterSize parameter into the field's match index" do
+      expect(subject["indexes"]["email_match"]["filterSize"]).to eq(512)
+    end
+
+    it "passes the filterTermBits parameter into the field's match index" do
+      expect(subject["indexes"]["email_match"]["filterTermBits"]).to eq(6)
+    end
+
+    it "does not pass the filter parameters into the field's exact index" do
+      expect(subject["indexes"]["email"]).to_not have_key("filterSize")
+      expect(subject["indexes"]["email"]).to_not have_key("filterTermBits")
+    end
+
+    it "does not pass the filter parameters into the field's range index" do
+      expect(subject["indexes"]["email_range"]).to_not have_key("filterSize")
+      expect(subject["indexes"]["email_range"]).to_not have_key("filterTermBits")
+    end
+
+    it "does not pass the filter parameters into another field's match index" do
+      expect(subject["indexes"]["first_name_match"]).to_not have_key("filterSize")
+      expect(subject["indexes"]["first_name_match"]).to_not have_key("filterTermBits")
+    end
+
   end
 end
